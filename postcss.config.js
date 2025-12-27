2f6af59e9549c5e793df6b1f414f87b9ddc59922
bmosesjorgensen <b.moses.jorgensen@gmail.com>
2025-12-26 16:42:06 -0800

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+✅ Commit.
+
+---
+
+## **📄 FILE 5: .gitignore**
+
+**Filename:** `.gitignore`
+```
+/node_modules
+/.next/
+/out/
+.DS_Store
+*.pem
+npm-debug.log*
+.env*.local
+.vercel
